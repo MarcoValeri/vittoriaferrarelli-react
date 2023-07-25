90f1859935b331532b7ab14062d0a2439e9c2e21
MarcoValeri <marcovaleri@hotmail.it>
2023-07-25 16:04:01 +0100

    Add responsive nav menu for mobile screens

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -5,18 +5,19 @@ import './Nav.scss';
 const Nav = () => {
     return (
         <nav className='nav'>
-            <div className="nav__wrapper">
-                <div className="nav__container-logo">
-                    <h2><Link className="nav__logo link-no-style h1" to="/">Vittoria Ferrarelli</Link></h2>
-                </div>
-                <div className="nav__container-menu">
-                    <ul className="list-menu">
-                        <li className="nav__menu-item"><Link className="body-md link-no-style" to="/">Home</Link></li>
-                        <li className="nav__menu-item"><Link className="body-md link-no-style" to="/chi-sono">Chi Sono</Link></li>
-                        <li className="nav__menu-item"><Link className="body-md link-no-style" to="/opere">Opere</Link></li>
-                        <li className="nav__menu-item"><Link className="body-md link-no-style" to="/contatti">Contatti</Link></li>
-                    </ul>
-                </div>
+            <div className="nav__toggle">
+                <label htmlFor="menu" aria-label="Main Navigation Menu"></label>
+                <input id="menu" className="nav__input" name="menu" type="checkbox" aria-label="Main Navigation Menu" />
+                <span className="nav__span nav__span--empty"></span>
+                <span className="nav__span"></span>
+                <span className="nav__span"></span>
+                <ul className="nav__menu list-menu">
+                    <li className="nav__container-logo"><h2><Link className="nav__logo link-no-style h1" to="/">Vittoria Ferrarelli</Link></h2></li>
+                    <li className="nav__menu-item"><Link className="body-md link-no-style" to="/">Home</Link></li>
+                    <li className="nav__menu-item"><Link className="body-md link-no-style" to="/chi-sono">Chi Sono</Link></li>
+                    <li className="nav__menu-item"><Link className="body-md link-no-style" to="/opere">Opere</Link></li>
+                    <li className="nav__menu-item"><Link className="body-md link-no-style" to="/contatti">Contatti</Link></li>
+                </ul>
             </div>
         </nav>
     )
