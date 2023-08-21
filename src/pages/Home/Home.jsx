04f0c19b67ec22a8420765568958aec804405d15
MarcoValeri <marcovaleri@hotmail.it>
2023-08-21 21:55:31 +0100

    Fix capital latter bug for the folder Home

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,17 @@
+import Footer from '../../components/Footer/Footer';
+import Nav from '../../components/Nav/Nav';
+import OperePreview from '../../components/OperePreview/OperePreview';
+
+import './Home.scss';
+
+const Home = () => {
+    return (
+        <div>
+            <Nav />
+            <OperePreview />
+            <Footer />
+        </div>
+    )
+}
+
+export default Home;
